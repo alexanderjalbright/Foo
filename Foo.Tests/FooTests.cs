e72a49d8067ec866905dd112eac7e2f308f5af76
Alex Albright <alexanderjalbright@gmail.com>
2019-01-21 09:44:51 -0500

    Initial Commit

---
+++ Foo.Tests/FooTests.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace Foo.Tests
+{
+    public class FooTests
+    {
+        [Fact]
+        public void Properties_Hold_Values()
+        {
+            // Arrange
+            Foo bar = new Foo();
+
+            // Act
+
+            // Assert
+        
+
+
+        }
+    }
+}
