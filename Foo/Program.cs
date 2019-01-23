1afc227fee0d4ca2f3ee4b6e1ed1e7d7dd0bcf72
Alex Albright <alexanderjalbright@gmail.com>
2019-01-23 18:58:38 -0500

    Added menu choices for display info

--- Foo/Program.cs
+++ Foo/Program.cs
@@ -23,18 +23,24 @@ namespace Foo
                         person.Add(CreateBar());
                         numPersons++;
                         break;
+
                     case "2":
+                        DisplayNameList(person);
                         Console.WriteLine("Who would you like to view? Type 0 for all.");
-                        int selection = Convert.ToInt32(Console.ReadLine()) - 1;
-                        if(selection != -1)
-                            person[selection].DisplayInfo();
-                        else
+                        int userSelection = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine(" ");
+                        int index = userSelection - 1;
+                        if(userSelection == 0)
                             DisplayAllPpl(person);
+                        else
+                            person[index].DisplayInfo();
                         break;
+
                     case "0":
                         Console.WriteLine("\nSee you next time!\n");
                         quit = true;
                         break;
+
                     default:
                         Console.WriteLine("\nUnaccepted selection. Try again.\n");
                         break;
@@ -74,6 +80,15 @@ namespace Foo
             for (int i = 0; i < person.Count; i++)
             {
                 person[i].DisplayInfo();
+            }
+        }
+
+        static void DisplayNameList(List<Bar> person)
+        {
+            for (int i = 0; i < person.Count; i++)
+            {
+                int userNum = i + 1;
+                Console.WriteLine(userNum + ". " + person[i].FirstName + " " + person[i].LastName);
             }
         }
 
